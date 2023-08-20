17ba34065e1d4f4da0a2564eb0b7617238aa54ba
zirakallaf <85106397+zirakallaf@users.noreply.github.com>
2023-08-20 11:09:18 +0100

    Update Program.cs

--- TextMatch/Program.cs
+++ TextMatch/Program.cs
@@ -1,17 +1,71 @@
+using System.Net;
+using TextMatch.Helpers;
+using TextMatch.Middlewares;
+using TextMatch.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+// configure cors
+string corsPolicy = "PolicyForClientApps";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(corsPolicy, policy =>
+    {
+        var origins = builder.Configuration.GetSection("AllowedHosts").Get<string>();
+
+        if (!string.IsNullOrEmpty(origins))
+        {
+            var originList = origins.Split(';');
+            policy.WithOrigins(originList);
+        }
+    });
+});
+
+
+var hstsConfig = builder.Configuration.GetRequiredSection("Hsts").Get<HstsConfig>();
+if (!builder.Environment.IsDevelopment() && hstsConfig != null)
+{
+    if (hstsConfig.IsHstsEnabled)
+    {
+        // Https port configuration
+        builder.Services.AddHttpsRedirection(options =>
+        {
+            var netHelper = new NetworkHelper();
+
+            options.RedirectStatusCode = (int)HttpStatusCode.PermanentRedirect;
+            var defaultHttpsPort = 443;
+            options.HttpsPort = netHelper.IsPortValid(hstsConfig.HttpsPort) ? hstsConfig.HttpsPort : defaultHttpsPort;
+        });
+
+        // HSTS configurations
+        builder.Services.AddHsts(options =>
+        {
+            options.Preload = hstsConfig.IsPreload;
+            options.IncludeSubDomains = hstsConfig.IncludeSubDomains;
+            options.MaxAge = new TimeSpan(hstsConfig.MaxAgeByDay);
+            foreach (var host in hstsConfig.ExcludedDomains)
+            {
+                options.ExcludedHosts.Add(host);
+            }
+        });
+    }
+}
+
 var app = builder.Build();
 
+app.UseCors(corsPolicy);
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+// Additional security layer added to header
+app.UseMiddleware<SecurityMiddleware>();
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
